202dffe34575d85dc66aa5d7f2df72816728a240
思忠 <chenbinbin.cb@alibaba-inc.com>
2019-07-11 14:26:11 +0800

    feat: kit3 support

---
+++ scripts/scaffolds.js
@@ -0,0 +1,8 @@
+const scaffolds = [
+  'admin-lite',
+
+  // TODO: 暂不升级
+  // 'd2-admin-ice',
+];
+
+module.exports = scaffolds;
--- scripts/upload-oss.js
+++ scripts/upload-oss.js
@@ -1,16 +1,21 @@
 const oss = require('ali-oss');
 const path = require('path');
+const fs = require('fs');
+const request = require('request');
+const scaffolds = require('./scaffolds');
+const pkgData = require('../package.json');
 
 const bucket = 'iceworks';
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const accessKeySecret = process.env.ACCESS_KEY_SECRET;
 const branch = process.env.TRAVIS_BRANCH;
-const assetsPath = branch === 'production' ? 'assets' : 'pre-assets';
+const dirPath = pkgData.kitVersion === '3.x' ? 'materials/' : '/';
+let assetsPath;
 
-if (['master', 'production'].indexOf(branch) === -1) {
-  console.log('当前分支非 master/production, 不执行物料源同步脚本');
-  console.log(`TRAVIS_BRANCH=${branch}`);
-  process.exit(0);
+if (['master', 'stable/kit-2.x'].indexOf(branch) === -1) {
+  assetsPath = 'pre-assets';
+} else {
+  assetsPath = 'assets';
 }
 
 const ossClient = oss({
@@ -21,11 +26,45 @@ const ossClient = oss({
   time: '120s',
 });
 
-const fromPath = path.resolve(__dirname, '../build/materials.json');
-const toPath = path.join(assetsPath, 'vue-materials.json');
+const materialPath = path.resolve(__dirname, '../build/materials.json');
+const toPath = path.join(assetsPath, dirPath, 'vue-materials.json');
 
-console.log('start upload oss', fromPath, toPath);
+console.log('start upload oss', materialPath, toPath);
 
-ossClient.put(toPath, fromPath).then((result) => {
-  console.log('upload success', result);
-});
+sortScaffoldMaterials()
+  .then(() => {
+    return ossClient.put(toPath, materialPath);
+  })
+  .then((result) => {
+    console.log('upload success', result);
+  });
+
+/**
+ * 按照下载量进行排序推荐
+ */
+function sortScaffoldMaterials() {
+  return new Promise((resolve, reject) => {
+    const materialsData = JSON.parse(fs.readFileSync(materialPath, 'utf-8'));
+
+    const sortMaterialsData = [];
+    scaffolds.forEach((scaffold) => {
+      materialsData.scaffolds.forEach((currentItem) => {
+        if (currentItem.name === scaffold) {
+          sortMaterialsData.push(currentItem);
+        }
+      });
+    });
+
+    materialsData.scaffolds = sortMaterialsData;
+
+    return fs.writeFile(
+      materialPath,
+      JSON.stringify(materialsData, null, 2),
+      'utf-8',
+      (err) => {
+        if (err) reject(err);
+        resolve();
+      }
+    );
+  });
+}
