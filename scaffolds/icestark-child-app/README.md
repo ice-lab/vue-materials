228e19bb09d8805ab9764aa567b9f5f51f0199e0
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-10-19 12:35:43 +0800

    feat: add icestark child app

---
+++ scaffolds/icestark-child-app/README.md
@@ -0,0 +1,3 @@
+# icestark Vue 子应用
+
+icestark 基于 Vue 的子应用模板
---
+++ scaffolds/icestark-child-app/src/pages/List/List.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>小二：列表页面</div>
+    <router-link to="/">home</router-link>
+    <br />
+    <router-link to="/detail">detail</router-link>
+  </div>
+</template>
