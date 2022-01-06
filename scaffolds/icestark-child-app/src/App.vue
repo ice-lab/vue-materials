5758c46c1595a0c27e6e3d2983b0635263a7b24d
maoxiaoke <maoxiaoke@outlook.com>
2022-01-06 15:14:10 +0800

    refactor: 💡 refact code

--- scaffolds/icestark-child-app/src/App.vue
+++ scaffolds/icestark-child-app/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import appHistory from '@ice/stark-app/lib/appHistory';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import appHistory from '@ice/stark-app/lib/appHistory';
+
 const jumpToLayout = () => {
   appHistory.push('/');
 };
